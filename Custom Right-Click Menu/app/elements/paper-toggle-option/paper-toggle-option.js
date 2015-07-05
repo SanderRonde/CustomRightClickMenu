e3a842530d07f8c65f9cf7e7aa7f1dcdf1690062
BarryBamibal <awsdfgvhbjn@gmail.com>
2015-07-05 15:18:53 +0200

    Added runOrAddAsCallback, which looks if the settings area already loaded, if not, the function gets executed when they are

--- Custom Right-Click Menu/app/elements/paper-toggle-option/paper-toggle-option.js
+++ Custom Right-Click Menu/app/elements/paper-toggle-option/paper-toggle-option.js
@@ -13,11 +13,14 @@ Polymer({
 	},
 
 	ready: function () {
-		this.toggled = options.settings[$(this).attr("id")];
+		var el = this;
+		runOrAddAsCallback(function() {
+			el.toggled = options.settings[$(el).attr('id')];
+		}, this);
 	},
 
 	onClick: function () {
-		var id = $(this).attr("id");
+		var id = $(this).attr('id');
 		this.toggled = !this.toggled;
 		console.log(id, this.toggled);
 		options.set(id, this.toggled);
