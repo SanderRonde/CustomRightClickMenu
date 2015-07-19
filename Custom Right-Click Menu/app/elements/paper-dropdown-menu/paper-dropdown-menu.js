84db0f459fca2d3d4f3da38dbea664f81030d6fd
Sander Ronde <awsdfgvhbjn@gmail.com>
2015-07-19 19:38:42 +0200

    Added a dropdown-menu for the script-edit element that allows you to pick one of a few options and shows you the currently selected option on collapse

---
+++ Custom Right-Click Menu/app/elements/paper-dropdown-menu/paper-dropdown-menu.js
@@ -0,0 +1,21 @@
+﻿Polymer({
+	is: 'paper-dropdown-menu',
+
+	behaviors: [Polymer.PaperDropdownBehavior],
+
+	/*
+	 * Fires when the selected item changes
+	 */
+	_dropdownSelectChange: function(_this) {
+		var paperItems = $(_this).find('paper-item');
+		var newState = _this._paperMenu.selected;
+		_this.$.dropdownSelected.innerHTML = paperItems[newState].children[1].innerHTML;
+	},
+
+	ready: function () {
+		var paperItems = $(this).find('paper-item');
+		console.log(this.selected);
+		this.$.dropdownSelected.innerHTML = paperItems[this.selected].children[1].innerHTML;
+		console.log(this.indent);
+	}
+});
