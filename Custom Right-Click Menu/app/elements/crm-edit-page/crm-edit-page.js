cd18637a635c1d33614b95d75485ef958378d144
Sander Ronde <awsdfgvhbjn@gmail.com>
2015-07-27 23:56:22 +0200

    Animations are now done with web-animations.js instead of jQuery which is a lot smoother.  Stored all the animations in properties so they can be called multiple times or reversed. Changed assiging "is[type]" to the 10% faster function.

--- Custom Right-Click Menu/app/elements/crm-edit-page/crm-edit-page.js
+++ Custom Right-Click Menu/app/elements/crm-edit-page/crm-edit-page.js
@@ -1,8 +1,9 @@
 ﻿'use strict';
-var crmEditPage;
 
 Polymer({
 	is: 'crm-edit-page',
+
+	behaviors: [Polymer.NeonAnimationRunnerBehavior],
 
 	/**
 	 * The item to edit
@@ -59,72 +60,177 @@ Polymer({
 	isMenu: false,
 
 	/**
+	 * The link item
+	 *
+	 * @attribute linkItem
+	 * @type Object
+	 * @default {}
+	 */
+	linkItem: {},
+
+	/**
+	 * The script item
+	 *
+	 * @attribute scriptItem
+	 * @type Object
+	 * @default {}
+	 */
+	scriptItem: {},
+
+	/**
+	 * The divider item
+	 *
+	 * @attribute dividerItem
+	 * @type Object
+	 * @default {}
+	 */
+	dividerItem: {},
+
+	/**
+	 * The menu item
+	 *
+	 * @attribute menuItem
+	 * @type Object
+	 * @default {}
+	 */
+	menuItem: {},
+
+	/**
+     * Whether the page is opened
+     * 
+     * @attribute opened
+     * @type Boolean
+     * @default false
+     */
+	opened: false,
+
+	/**
+     * The overlay element associated with the current dialog
+     * 
+     * @attribute $overlayEl
+     * @type Element
+     * @default null
+     */
+	$overlayEl: null,
+
+	/**
+	 * The overlayEl animation
+	 *
+	 * @attribute overlayAnimation
+	 * @type Animation
+	 * @default null
+	 */
+	overlayAnimation: null,
+
+	properties: {
+		animationConfig: {
+			value: function() {
+				return {
+					'entry': {
+						name: 'scale-up-animation',
+						node: this.$.editPageCont,
+						duration: 300
+					},
+					'exit': {
+						name: 'scale-down-animation',
+						node: this.$.editPageCont,
+						duration: 300
+					}
+				}
+			}
+		}
+	},
+
+	listeners: {
+		"neon-animation-finish": '_onNeonAnimationFinish'
+	},
+
+	_onNeonAnimationFinish: function () {
+		var _this = this;
+		if (this.opened) {
+			console.log('end', 'opened');
+			this.$overlayEl.on('click', function () {
+				$(_this.$.editPageCont).children('link-edit, script-edit, divider-edit, menu-edit')[0].cancelChanges();
+			});
+		}
+		else {
+			console.log('end');
+			this.$overlayEl[0].style.display = 'none';
+			this.$.editPageCont.style.display = 'none';
+			$(_this.$.editPageCont).children('link-edit, script-edit, divider-edit, menu-edit')[0].removeChanges();
+			options.show = false;
+		}
+	},
+
+	/**
 	 * @param eventSourceElement The element that was clicked on
 	 */
 	animateIn: function () {
-		var _this = this;
-		setTimeout(function() {
-			var overlayEl = $('.overlayCont')[0];
-			overlayEl.style.display = 'block';
-			options.show = true;
-			var overlayCont = $('.overlayCont')[0];
-			$(_this.$.editPageCont).stop().animate({
-				'opacity': 1
+		this.$overlayEl.css('display', 'block');
+		(this.overlayAnimation && this.overlayAnimation.play()) || (this.overlayAnimation = this.$overlayEl[0].animate([
+			{
+				opacity: 0
 			}, {
-				'easing': 'easeInCubic',
-				'duration': 300,
-				'step': function(progress) {
-					this.style.transform = 'scale(' + progress + ')';
-					overlayCont.style.opacity = progress / 5;
-				},
-				'complete': function() {
-					$(overlayCont).on('click', function() {
-						$(_this.$.editPageCont).children('link-edit, script-edit, divider-edit, menu-edit')[0].cancelChanges();
-					});
-				}
-			});
-		}, 150);
+				opacity: 0.3
+			}
+		], {
+			duration: 300,
+			fill: 'both',
+			easing: 'cubic-bezier(0.215, 0.610, 0.355, 1.000)'
+		}));
+			
+		options.show = true;
+		this.opened = true;
+		this.$.editPageCont.style.display = 'block';
+		this.playAnimation('entry');
 	},
 	
 	animateOut: function () {
-		var _this = this;
-		var overlayEl = $('.overlayCont').off('click')[0];
-		var overlayCont = $('.overlayCont')[0];
-		$(this.$.editPageCont).stop().animate({
-			'opacity': 0
-		}, {
-			'easing': 'easeInCubic',
-			'duration': 300,
-			'step': function(progress) {
-				this.style.transform = 'scale(' + progress + ')';
-				overlayCont.style.opacity = progress / 5;
-			},
-			'complete': function() {
-				this.style.transform = 'scale(0)';
-				this.style.opacity = 1;
-				overlayEl.style.opacity = 0;
-				overlayEl.style.display = 'none';
-				$(_this.$.editPageCont).children('link-edit, script-edit, divider-edit, menu-edit')[0].removeChanges();
-				options.show = false;
-			}
-		});
+		this.overlayAnimation.reverse();
+		this.$overlayEl.off('click');
+		this.playAnimation('exit');
+		this.opened = false;
 	},
 
 	ready: function () {
 		$('.popupCont').click(function(e) {
 			e.stopPropagation();
 		});
-		this.init();
+		this.$overlayEl = $('.overlayCont');
+		window.crmEditPage = this;
+		this.isLink = this.isMenu = this.isScript = this.isDivider = false;
 	},
 	
 	init: function() {
-		crmEditPage = this
+		console.trace();
+		var _this = this;
+		var valueStorer = {};
+		this.scriptItem = this.linkItem = this.dividerItem = this.menuItem = {};
+		if ((valueStorer.isScript = this.item.type === 'script')) {
+			this.scriptItem = this.item;
+			valueStorer.isLink = valueStorer.isMenu = valueStorer.isDivider = false;
+		}
+		else if ((valueStorer.isLink = this.item.type === 'link')) {
+			this.linkItem = this.item;
+			valueStorer.isMenu = valueStorer.isDivider = false;
+		}
+		else if ((valueStorer.isMenu = this.item.type === 'menu')) {
+			this.menuItem = this.item;
+			valueStorer.isDivider = false;
+		}
+		else {
+			valueStorer.isDivider = true;
+			this.dividerItem = this.item;
+
+		}
 		setTimeout(function() {
-			crmEditPage.isLink = (crmEditPage.item.type === 'link');
-			crmEditPage.isMenu = (crmEditPage.item.type === 'menu');
-			crmEditPage.isScript = (crmEditPage.item.type === 'script');
-			crmEditPage.isDivider = (crmEditPage.item.type === 'divider');
-			crmEditPage.animateIn();
-		}, 100);
+			window.options.show = true;
+			_this.isScript = valueStorer.isScript;
+			_this.isLink = valueStorer.isLink;
+			_this.isMenu = valueStorer.isMenu;
+			_this.isDivider = valueStorer.isDivider;
+			$(_this).find('#editPageCont > :not([hidden])')[0].init();
+			_this.animateIn();
+		}, 300);
 	}
 });
