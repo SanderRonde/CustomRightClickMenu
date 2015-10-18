d43e10449ce84881b0831e9589851e524daf80f5
Sander Ronde <awsdfgvhbjn@gmail.com>
2015-10-18 21:16:44 +0200

    Made an attempt at making the project ready for building by using the straight bower source and crisping it, this is pretty tough since the crisper plugin doesn't allow for multiple files so i'll look at it again later. Removed some code from a previous project

--- Custom Right-Click Menu/gruntfile.js
+++ Custom Right-Click Menu/gruntfile.js
@@ -1,59 +1,25 @@
-﻿module.exports = function (grunt) {
+﻿var sources = ['bower_components_uncrisp/webcomponentsjs/webcomponents.min.js','bower_components_uncrisp/polymer/polymer-micro.html', 'bower_components_uncrisp/polymer/polymer-mini.html', 'bower_components_uncrisp/polymer/polymer.html', 'bower_components_uncrisp/paper-toolbar/paper-toolbar.html', 'bower_components_uncrisp/iron-icons/iron-icons.html', 'bower_components_uncrisp/paper-drawer-panel/paper-drawer-panel.html', 'bower_components_uncrisp/paper-input/paper-input.html', 'bower_components_uncrisp/paper-toast/paper-toast.html', 'bower_components_uncrisp/paper-ripple/paper-ripple.html', 'bower_components_uncrisp/paper-menu/paper-menu.html', 'bower_components_uncrisp/paper-item/paper-item.html', 'bower_components_uncrisp/paper-input/paper-textarea.html', 'bower_components_uncrisp/paper-icon-button/paper-icon-button.html', 'bower_components_uncrisp/paper-radio-button/paper-radio-button.html', 'bower_components_uncrisp/paper-radio-group/paper-radio-group.html', 'bower_components_uncrisp/paper-dialog/paper-dialog.html ', 'bower_components_uncrisp/paper-spinner/paper-spinner.html', 'bower_components_uncrisp/paper-toggle-button/paper-toggle-button.html', 'bower_components_uncrisp/neon-animation/animations/scale-down-animation.html ', 'bower_components_uncrisp/neon-animation/animations/fade-out-animation.html', 'bower_components_uncrisp/neon-animation/animations/scale-up-animation.html', 'bower_components_uncrisp/paper-styles/paper-styles.html', 'bower_components_uncrisp/iron-icon/iron-icon.html', 'bower_components_uncrisp/iron-iconset-svg/iron-iconset-svg.html', 'bower_components_uncrisp/iron-media-query/iron-media-query.html', 'bower_components_uncrisp/iron-selector/iron-selector.html', 'bower_components_uncrisp/paper-drawer-panel/paper-drawer-panel.css', 'bower_components_uncrisp/iron-input/iron-input.html', 'bower_components_uncrisp/iron-form-element-behavior/iron-form-element-behavior.html', 'bower_components_uncrisp/paper-input/paper-input-behavior.html', 'bower_components_uncrisp/paper-input/paper-input-container.html', 'bower_components_uncrisp/paper-input/paper-input-error.html', 'bower_components_uncrisp/paper-input/paper-input-char-counter.html', 'bower_components_uncrisp/paper-styles/typography.html', 'bower_components_uncrisp/iron-a11y-announcer/iron-a11y-announcer.html', 'bower_components_uncrisp/iron-a11y-keys-behavior/iron-a11y-keys-behavior.html', 'bower_components_uncrisp/iron-menu-behavior/iron-menu-behavior.html', 'bower_components_uncrisp/paper-menu/paper-menu-shared.css', 'bower_components_uncrisp/iron-behaviors/iron-control-state.html', 'bower_components_uncrisp/iron-behaviors/iron-button-state.html', 'bower_components_uncrisp/paper-item/paper-item-shared-styles.html', 'bower_components_uncrisp/iron-autogrow-textarea/iron-autogrow-textarea.html', 'bower_components_uncrisp/iron-flex-layout/iron-flex-layout.html', 'bower_components_uncrisp/paper-styles/default-theme.html', 'bower_components_uncrisp/paper-behaviors/paper-button-behavior.html', 'bower_components_uncrisp/paper-behaviors/paper-inky-focus-behavior.html', 'bower_components_uncrisp/iron-checked-element-behavior/iron-checked-element-behavior.html', 'bower_components_uncrisp/iron-selector/iron-selectable.html', 'bower_components_uncrisp/neon-animation/neon-animation-runner-behavior.html', 'bower_components_uncrisp/paper-dialog-behavior/paper-dialog-behavior.html', 'bower_components_uncrisp/paper-dialog-behavior/paper-dialog-common.css', 'bower_components_uncrisp/paper-styles/color.html', 'bower_components_uncrisp/paper-spinner/paper-spinner.css', 'bower_components_uncrisp/neon-animation/neon-animation-behavior.html', 'bower_components_uncrisp/neon-animation/web-animations.html','bower_components_uncrisp/iron-flex-layout/classes/iron-flex-layout.html','bower_components_uncrisp/paper-styles/shadow.html','bower_components_uncrisp/iron-meta/iron-meta.html','bower_components_uncrisp/iron-validatable-behavior/iron-validatable-behavior.html','bower_components_uncrisp/paper-input/paper-input-addon-behavior.html','bower_components_uncrisp/font-roboto/roboto.html','bower_components_uncrisp/iron-selector/iron-multi-selectable.html','bower_components_uncrisp/iron-selector/iron-selection.html','bower_components_uncrisp/neon-animation/neon-animatable-behavior.html','bower_components_uncrisp/iron-overlay-behavior/iron-overlay-behavior.html','bower_components_uncrisp/web-animations-js/web-animations-next-lite.min.js','bower_components_uncrisp/iron-flex-layout/classes/iron-shadow-flex-layout.html','bower_components_uncrisp/neon-animation/animations/opaque-animation.html','bower_components_uncrisp/iron-fit-behavior/iron-fit-behavior.html','bower_components_uncrisp/iron-resizable-behavior/iron-resizable-behavior.html','bower_components_uncrisp/iron-overlay-behavior/iron-overlay-backdrop.html','bower_components_uncrisp/iron-overlay-behavior/iron-overlay-manager.html'];
+var toCopy = [];
+var i;
+var obj;
+var fileObjects = [];
+for (i = 0; i < sources.length; i++) {
+	obj = {
+		src: 'app/' + sources[i],
+		dest: 'app/' + sources[i].replace('bower_components_uncrisp', 'bower_components_test')
+	};
+	fileObjects.push(obj);
+}
+
+module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		htmlmin: {
-			dist: {
+		crisper: {
+			updateBower: {
 				options: {
-					removeComments: false,
-					collapseWhitespace: true
+					cleanup: false
 				},
-				files: {
-					'build/window.html': 'window.html'
-				}
-			}
-		},
-		minified: {
-			files: {
-				src: [
-					'Scripts/*.js'
-				],
-				dest: 'build/Scripts/'
-			}
-		},
-		cssmin: {
-			options: {
-				shorthandCompacting: false,
-				roundinPrecision: -1
-			},
-			targets: {
-				files: [{
-					expand: true,
-					src: ['css.css', 'polymer.css'],
-					dest: 'build/',
-					ext: '.css'
-				}]
-			}
-		},
-		copy: {
-			main: {
-				files: [
-					{ expand: true, src: ['Scripts/jquery-2.0.3.min.js', 'Scripts/jquery.color-2.1.2.min.js', 'jquery.easing.js'], dest: 'build/' },
-					{ expand: true, src: ['Images/*'], dest: 'build/' },
-					{ expand: true, src: ['Colorpicker/**'], dest: 'build/' },
-					{ expand: true, src: ['icon-large.png', 'icon-small.png', 'icon-supersmall.png', 'manifest.json', 'Segoe_UI.ttf'], dest: 'build/' }
-				]
-			}
-		},
-		usebanner: {
-			taskName: {
-				options: {
-					position: 'top',
-					banner: '/*Original can be found at https://github.com/BarryBamibal/BinderApp */',
-					linebreak: true
-				},
-				files: {
-					src: ['build/Scripts/background.js', 'build/Scripts/js.js', 'build/Scripts/polymer.js', 'build/Scripts/searchWorker.js', 'build/Scripts/worker.js', 'build/css.css', 'build/polymer.css']
-				}
+				files: fileObjects
 			}
 		}
 	});
@@ -63,6 +29,8 @@
 	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-banner');
+	grunt.loadNpmTasks('grunt-crisper');
 
-	grunt.registerTask('build', ['htmlmin', 'minified', 'cssmin', 'copy', 'usebanner']);
+	grunt.registerTask('updateBower', ['crisper']);
+	grunt.registerTask('build', []);
 }
