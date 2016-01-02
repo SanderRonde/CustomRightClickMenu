b2b1008357e681a726b55548de9c9e07a191971d
Sander Ronde <awsdfgvhbjn@gmail.com>
2016-01-02 22:03:47 +0100

    device switch

---
+++ Custom Right-Click Menu/app/elements/menu-edit/menu-edit.js
@@ -0,0 +1,22 @@
+﻿Polymer({
+	is: 'menu-edit',
+
+	behaviors: [Polymer.NodeEditBehavior],
+
+	properties: {
+		item: {
+			type: Object,
+			value: {},
+			notify: true
+		}
+	},
+
+	init: function () {
+		this._init();
+	},
+
+	ready: function () {
+		window.menuEdit = this;
+	}
+
+});
