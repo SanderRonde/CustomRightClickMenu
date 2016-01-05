cebafab3886d6942aa525b2c7c5dcbbb62153cb6
Sander Ronde <awsdfgvhbjn@gmail.com>
2016-01-05 17:49:41 +0100

    default-link now has support for searchengines and uses a blue buttn

--- Custom Right-Click Menu/app/elements/default-link/default-link.js
+++ Custom Right-Click Menu/app/elements/default-link/default-link.js
@@ -1,6 +1,15 @@
 ﻿Polymer({
 	is: 'default-link',
 
+	/**
+	 * Whether the link is a search-engine
+	 * 
+	 * @param searchengine
+	 * @type Boolean
+	 * @value false
+	 */
+	searchengine: false,
+
 	properties: {
 		href: {
 			type: String,
@@ -15,11 +24,37 @@
 	onClick: function() {
 		var link = this.href;
 		var name = $(this.$.input).val();
-		var newItem = {
-			name: name,
-			type: 'link',
-			value: link
-		};
-		options.crm.add(newItem);
+		var script = '' +
+			'var query;\n' +
+			'var url = ' + link + '\n' +
+			'if (crmAPI.getSelection()) {\n' +
+			'	query = crmAPI.getSelection();\n' +
+			'} else {\n' +
+			'	query = window.prompt(\'Please enter a search query\');\n' +
+			'}\n' +
+			'if (query) {\n' +
+			'	window.open(url.replace(/%s/g,query), \'_blank\');\n' +
+			'}\n';
+
+	var newItem;
+		if (this.searchengine) {
+			newItem = window.app.templates.getDefaultScriptNode({
+				name: name,
+				value: {
+					script: script
+				}
+			});
+		} else {
+			newItem = window.app.templates.getDefaultLinkNode({
+				name: name,
+				value: [
+					{
+						url: link,
+						newTab: true
+					}
+				]
+			});
+		}
+		app.crm.add(newItem);
 	}
 })
