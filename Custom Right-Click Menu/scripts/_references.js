7f86a8f5b950d998c909779b93124d9e110832eb
Sander Ronde <awsdfgvhbjn@gmail.com>
2016-01-05 17:55:44 +0100

    changed options to app

--- Custom Right-Click Menu/scripts/_references.js
+++ Custom Right-Click Menu/scripts/_references.js
@@ -3,14 +3,14 @@
 /// <reference path="jquery-2.1.4.min.map"/>
 /// <reference path="/app/js/jquery.contextMenu.js" />
 /// <reference path="../app/elements/crm-edit-page/crm-edit-page.js" />
-/// <reference path="../app/elements/crm-options/crm-options.js" />
+/// <reference path="../app/elements/crm-app/crm-app.js" />
 /// <reference path="../app/elements/default-link/default-link.js" />
 /// <reference path="../app/elements/edit-crm/edit-crm.js" />
 /// <reference path="../app/elements/edit-crm-item/edit-crm-item.js" />
-/// <reference path="../app/elements/link-edit/link-edit.js" />
+/// <reference path="~/app/elements/edit-pages/link-edit/link-edit.js" />
 /// <reference path="../app/elements/paper-dropdown-menu/paper-dropdown-menu.js" />
 /// <reference path="../app/elements/paper-toggle-option/paper-toggle-option.js" />
-/// <reference path="../app/elements/script-edit/script-edit.js" />
+/// <reference path="~/app/elements/edit-pages/script-edit/script-edit.js" />
 /// <reference path="../app/elements/type-switcher/type-switcher.js" />
 /// <reference path="../app/js/codemirror.js" />
 /// <reference path="../app/js/jquery-ui.min.js" />
