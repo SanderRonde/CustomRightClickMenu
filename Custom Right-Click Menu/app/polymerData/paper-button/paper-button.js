6f1b5a9a8536f4b074af6c8c089493e3bd331e51
Sander Ronde <awsdfgvhbjn@gmail.com>
2016-04-21 22:10:34 +0200

    Added some more elements to facilitate a paper-button. Edited the dialog to the correct link and the correcct message and added a "copy settings" button

---
+++ Custom Right-Click Menu/app/polymerData/paper-button/paper-button.js
@@ -0,0 +1,40 @@
+
+
+  Polymer({
+
+    is: 'paper-button',
+
+    behaviors: [
+      Polymer.PaperButtonBehavior
+    ],
+
+    properties: {
+
+      /**
+       * If true, the button should be styled with a shadow.
+       */
+      raised: {
+        type: Boolean,
+        reflectToAttribute: true,
+        value: false,
+        observer: '_calculateElevation'
+      }
+    },
+
+    _calculateElevation: function() {
+      if (!this.raised) {
+        this._elevation = 0;
+      } else {
+        Polymer.PaperButtonBehaviorImpl._calculateElevation.apply(this);
+      }
+    },
+
+    _computeContentClass: function(receivedFocusFromKeyboard) {
+      var className = 'content ';
+      if (receivedFocusFromKeyboard) {
+        className += ' keyboard-focus';
+      }
+      return className;
+    }
+  });
+
