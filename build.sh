05a6d6ddd36930d823658b6074360497cd7f0ed8
SanderRonde <awsdfgvhbjn@gmail.com>
2016-04-26 22:25:13 +0200

    Added fancy logs

--- build.sh
+++ build.sh
@@ -1,18 +1,36 @@
+function logStart {
+  echo -e "\e[1m \e[34m $1"
+}
+
+function logSuccess {
+  echo -e "\e[1m \e[32m $1"
+}
+
+function logLog {
+  echo -e "\e[1m \e[33m $1"
+}
+
 set -e
 
 cd "Custom Right-Click Menu"
 
+logStart "Starting grunt tests";
 grunt test
+logSuccess "Finished grunt tests";
+
+logStart "Changing branches";
 grunt website
 
 git config user.name "Travis CI"
 git config user.email "awsdfgvhbjn@gmail.com"
+git config --global push.default simple
 
 git remote set-branches --add origin gh-pages
 git fetch
 
 git reset --hard
 git checkout -b gh-pages --track origin/gh-pages
+logSuccess "Changed branches";
 
 npm install
 
@@ -23,22 +41,24 @@ set +e
 git diff-index --quiet HEAD
 
 changes=$?;
-echo $changes;
 if [ $changes -eq 0 ] ; then #No changes
-  echo "No changes to the website were made";
+  logLog "No changes to the website were made";
   exit 0;
 else
-  echo "Changes occurred";
+  logLog "Changes occurred";
+  git diff;
 fi
 
-git add .
-git commit -m "Deploy to Github Pages"
+logStart "Committing changes";
+git add . --quiet
+git commit -m "Deploy to Github Pages" --quiet
+logSuccess "Committed changes";
 
-echo "Git commit exit code:";
-echo $?;
+logStart "Pushing changes";
 if [ $? -ne 0 ] ; then #Something went wrong committing, don't push
-  echo "Faulty commit, abort push";
+  logLog "Faulty commit, abort push";
   exit 0;
 fi
 
-git push "https://${GITHUB_ACCESS_TOKEN}@github.com/SanderRonde/CustomRightClickMenu.git" 
+git push "https://${GITHUB_ACCESS_TOKEN}@github.com/SanderRonde/CustomRightClickMenu.git" --quiet 
+logSuccess "Pushed github pages to branch";
