6417cd9a6444cc1b55316d532462c28c23c8ab73
Sander Ronde <awsdfgvhbjn@gmail.com>
2016-10-16 00:25:47 +0200

    doesn't clear when url ends with #noClear (for testing)

--- app/elements/crm-app/crm-app.js
+++ app/elements/crm-app/crm-app.js
@@ -2510,7 +2510,9 @@
 						document.documentElement.classList.remove('elementsLoading');
 
 						//Clear the annoying CSS mime type messages and the /deep/ warning
-						!window.lastError && console.clear();
+						if (!window.lastError && location.hash !== '#noClear') {
+							console.clear();
+						}
 
 						console.log('%cHey there, if you\'re interested in how this extension works check out the github repository over at https://github.com/SanderRonde/CustomRightClickMenu',
 							'font-size:120%;font-weight:bold;');
