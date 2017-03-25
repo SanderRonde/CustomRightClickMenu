7c20951b99e87853e4ed44573bfc48a4bd843510
SanderRonde <awsdfgvhbjn@gmail.com>
2017-03-25 04:22:27 +0100

    updated changelog

--- app/elements/change-log/changelog.js
+++ app/elements/change-log/changelog.js
@@ -7,8 +7,12 @@
 			'Added the CRM-API for more features',
 			'Added a stylesheet element',
 			'Added the ability to only show certain nodes on specified websites and/or content types',
-			'Added the ability to drag nodes',
-			'Made it possible to delete or export selected nodes'
+			'Added the ability to reposition nodes',
+			'Made it possible to delete or export selected nodes',
+			'Added compatability with Grease/Tamper-Monkey UserScripts',
+			'Added compatability with UserStylesheets',
+			'Added the ability to connect a script/stylesheet to a local editor',
+			'Now uses the better CodeMirror editor'
 		]
 	};
 }());
