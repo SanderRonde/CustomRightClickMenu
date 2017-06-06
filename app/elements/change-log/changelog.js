dee524cc2f14fd007bbd7613e254c0959886a74b
SanderRonde <awsdfgvhbjn@gmail.com>
2017-06-06 03:01:43 +0200

    updated changelog

--- app/elements/change-log/changelog.js
+++ app/elements/change-log/changelog.js
@@ -45,7 +45,8 @@
 			'Fixed a bug in the crmAPI.GM.GM_notification function'
 		],
 		'2.0.11': [
-			'Fixed the Â character appearing in the editors'
+			'Fixed the Â character appearing in the editors',
+			'Added an indicator for when the exporting field is slow'
 		]
 	};
 }());
