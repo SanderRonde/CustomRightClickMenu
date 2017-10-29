9f8c7dd3373fe9ee51a5d83f318cade731d4759a
SanderRonde <awsdfgvhbjn@gmail.com>
2017-10-29 04:03:14 +0100

    replaced chai.d.ts with actual @types package

--- test/UITest.ts
+++ test/UITest.ts
@@ -1,5 +1,4 @@
 /// <reference path="../tools/definitions/selenium-webdriver.d.ts" />
-/// <reference path="../tools/definitions/chai.d.ts" />
 /// <reference path="../tools/definitions/chrome.d.ts" />
 /// <reference path="../tools/definitions/crm.d.ts" />
 /// <reference path="../app/js/background.ts" />
@@ -2360,13 +2359,13 @@ describe('Options Page', function() {
 				}).then(() => {
 					return getSyncSettings(driver);
 				}).then((settings) => {
-					assert.strictEqual(settings.editor.tabSize, ~~newTabSize,
+					assert.strictEqual(~~settings.editor.tabSize, ~~newTabSize,
 						'tab size has changed to the correct number');
 					
 					reloadPage(this, driver).then(() => {
 						return getSyncSettings(driver);
 					}).then((settings) => {
-						assert.strictEqual(settings.editor.tabSize, ~~newTabSize,
+						assert.strictEqual(~~settings.editor.tabSize, ~~newTabSize,
 							'tab size has changed to the correct number');
 						done();
 					});
@@ -2657,7 +2656,7 @@ describe('Options Page', function() {
 					reloadPage(this, driver).then(() => {
 						return getCRM(driver);
 					}).then((crm) => {
-						assert.lengthOf(crm[0].value, 4, 'node has 4 links now');
+						assert.lengthOf(crm[0].value as Array<CRM.LinkNodeLink>, 4, 'node has 4 links now');
 
 						//Only one newTab can be false at a time
 						const newLinks = Array.apply(null, Array(4))
