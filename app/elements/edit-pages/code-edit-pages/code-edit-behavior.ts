101be3f95c9dc9ec33072e463da9a84727284e05
SanderRonde <awsdfgvhbjn@gmail.com>
2017-11-07 21:28:03 +0100

    searches for elements in correct shadowroot

--- app/elements/edit-pages/code-edit-pages/code-edit-behavior.ts
+++ app/elements/edit-pages/code-edit-pages/code-edit-behavior.ts
@@ -130,11 +130,11 @@ class CEB {
 		}
 		window.useOptionsCompletions = false;
 		this.hideCodeOptions();
-		Array.prototype.slice.apply(window.app.shadowRoot.querySelectorAll('.editorTab')).forEach(
+		Array.prototype.slice.apply(this.$$('.editorTab')).forEach(
 			function(tab: HTMLElement) {
 				tab.classList.remove('active');
 			});
-		window.app.shadowRoot.querySelector('.mainEditorTab').classList.add('active');
+		this.$$('.mainEditorTab').classList.add('active');
 	};
 
 		/**
