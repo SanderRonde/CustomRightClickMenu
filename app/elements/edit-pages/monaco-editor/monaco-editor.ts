5edff4185700c1107cb0ec4d8b3687243cd41bb2
SanderRonde <awsdfgvhbjn@gmail.com>
2017-11-10 23:44:40 +0100

    completed monaco editor element

--- app/elements/edit-pages/monaco-editor/monaco-editor.ts
+++ app/elements/edit-pages/monaco-editor/monaco-editor.ts
@@ -3,27 +3,57 @@
 class MOE {
 	static is: string = 'monaco-editor';
 
+	/**
+	 * A promise keeping track of the status of the editor
+	 */
+	static _monacoReady: Promise<void>;
+
+	/**
+	 * The editor associated with this element
+	 */
+	static editor: monaco.editor.IStandaloneCodeEditor;
+
 	static setupRequire() {
-		window.require.config({
-			paths: {
-				'vs': '../monaco-editor/min/vs'
-			}
-		});
-		window.require(['vs/editor/editor.main'], () => {
-			this._setMonacoListener();
+		this._monacoReady = new window.Promise<void>(async (resolve) => {
+			await window.onExists('require');
+			window.require.config({
+				paths: {
+					'vs': '../monaco-editor/min/vs'
+				}
+			});
+			window.require(['vs/editor/editor.main'], () => {
+				resolve(null);	
+			});
 		});
 	}
 
+	private static _fixThemeScope(this: MonacoEditor) {
+		window.app.monacoStyleElement = window.app.monacoStyleElement || 
+			document.getElementsByClassName('monaco-colors')[0] as HTMLStyleElement;
+		
+		if (this.shadowRoot.children[0] !== window.app.monacoStyleElement) {
+			this.shadowRoot.insertBefore(window.app.monacoStyleElement, this.shadowRoot.children[0]);
+		}
+	}
+
+	static create(this: MonacoEditor, options?: monaco.editor.IEditorConstructionOptions,
+		override?: monaco.editor.IEditorOverrideServices): Promise<MonacoEditor> {
+			return new Promise((resolve) => {
+				const listener = monaco.editor.onDidCreateEditor((editor) => {
+					listener.dispose();
+					this._fixThemeScope();
+					resolve(this);
+				});
+				this.editor = monaco.editor.create(this.$.container, options, override);
+			});
+		}
+
+	static destroy(this: MonacoEditor) {
+		this.editor.dispose();
+	}
+
 	static ready(this: MonacoEditor) {
-		window.require.config({
-			paths: {
-				'vs': '../monaco-editor/min/vs'
-			}
-		});
-		window.require(['vs/editor/editor.main'], () => {
-			this._setMonacoListener();
-		});
-		const editor = monaco.editor.create(null);
+		this.setupRequire();
 	}
 }
 
