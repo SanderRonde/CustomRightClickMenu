b8b823463ef9f4e624dd146a8cf0ed2e3ea939e1
SanderRonde <awsdfgvhbjn@gmail.com>
2017-12-26 01:04:42 +0100

    doesn't register jquery twice

--- app/js/libraries/jquery/jquery-2.0.3.js
+++ app/js/libraries/jquery/jquery-2.0.3.js
@@ -8815,9 +8815,6 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 	// derived from file names, and jQuery is normally delivered in a lowercase
 	// file name. Do this after creating the global so that if an AMD module wants
 	// to call noConflict to hide this version of jQuery, it will work.
-	if ( typeof define === "function" && define.amd ) {
-		define( "jquery", [], function () { return jQuery; } );
-	}
 }
 
 // If there is a window object, that at least has a document property,
