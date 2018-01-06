06a9ab2c04f9c928d0ee524277c6d7864f88da28
SanderRonde <awsdfgvhbjn@gmail.com>
2018-01-06 21:17:20 +0100

    included a set polyfill

---
+++ app/js/polyfills/set.ts
@@ -0,0 +1,88 @@
+const argumentsName = Object.prototype.toString.call((function() {
+	return arguments;
+}));
+
+class SetIterator<T> implements Iterator<T> {
+	private _index = 0;
+	constructor(private _data: Array<T>) { }
+	next() {
+		const val = this._data[this._index];
+		this._index++;
+		return val;
+	}
+}
+
+class Set<T> {
+	private _data: Array<T> = [];
+
+	length: 0;
+	constructor(iterable?: Iterable<T>) {
+		if (!(this instanceof Set)) {
+			throw new TypeError('Constructor requires new');
+		}
+		if (iterable && !this._isIterable(iterable)) {
+			throw new TypeError('Passed value is not iterable');
+		}
+		Object.defineProperty(this, 'size', {
+			get: this._getSize
+		});
+		if (iterable) {
+			Array.prototype.slice.apply(iterable).forEach((data: T) => {
+				this.add(data);
+			});
+		}
+	}
+	add(value: T) {
+		if (this._data.indexOf(value) === -1) {
+			this._data.push(value);
+		}
+		return this;
+	}
+	clear() {
+		while (this._data.length > 0) {
+			this._data.pop();
+		}
+		return this;
+	}
+	delete(value: T) {
+		if (this.has(value)) {
+			this._data.splice(this._data.indexOf(value, 1));
+			return true;
+		}
+		return false;
+	}
+	entries(): Iterator<[T, T]> {
+		return new SetIterator(this._data.map((data) => {
+			return [data, data];
+		}));
+	}
+	forEach<U = any>(callbackFn: (this: U, value: T) => void, thisArg?: U) {
+		this._data.forEach((data) => {
+			if (thisArg !== void 0) {
+				callbackFn.apply(thisArg, [data]);
+			} else {
+				(callbackFn as any)(data);
+			}
+		});
+	}
+	has(value: T) {
+		return this._data.indexOf(value) > -1;
+	}
+	keys() {
+		return this.values();
+	}
+	values() {
+		return new SetIterator(this._data);
+	}
+	
+	private _getSize(): number {
+		return this._data.length;
+	}
+	private _isIterable(value: Iterable<T>): value is Iterable<T> {
+		if (Array.isArray(value) || typeof value === 'string' || 
+			value.toString() === 'argumentsName') {
+				return true;
+			}
+		return false;
+	}
+}
