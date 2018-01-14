adb6cf7e914f74f9f7bf06e837a854bb86fe1cd8
SanderRonde <awsdfgvhbjn@gmail.com>
2018-01-14 04:03:01 +0100

    chooses es version depending on whether classes are supported

--- app/js/polyfills/es-loader.ts
+++ app/js/polyfills/es-loader.ts
@@ -1,17 +1,20 @@
 (() => {
-	const CHROME_ES3 = 26;
+    function supportsClasses() {
+        try {
+            eval('class TESTCLASS {}');
+            return true;
+        } catch(e) {
+            return false;
+        }
+    }
 
-	const chromeVersion = ~~/Chrome\/([0-9.]+)/.exec(navigator.userAgent)[1].split('.')[0];
-	const tag = document.createElement('script');
-	if (chromeVersion > CHROME_ES3) {
-		//Load es5/6
-		tag.src = 'options.js';
-	} else {
-		//Load es3 instead
-		tag.src = 'options.es3.js';
-	}
-
-	document.addEventListener('DOMContentLoaded', () => {
-		document.body.appendChild(tag);
-	});
+    const tag = document.createElement('script');
+    if (supportsClasses()) {
+        tag.src = 'options.js';
+    } else {
+        tag.src = 'options.es3.js';
+    }
+    document.addEventListener('DOMContentLoaded', () => {
+        document.body.appendChild(tag);
+    });
 })();
