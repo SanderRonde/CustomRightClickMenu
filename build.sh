03c3ca5c2eadf92e1a1c4028567a6818311d6380
SanderRonde <awsdfgvhbjn@gmail.com>
2018-02-25 05:11:34 +0100

    locked typescript version to prevent compile errors

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 http-server -p 1234 . -s &
 
-npm install -g typescript@latest
+npm install -g typescript@2.6.2
 
 echo "Typescript Compiling App" && echo -en "travis_fold:start:ts_app\\r"
 tsc
