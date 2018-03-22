689f3c45df4734eae1000d5f96722a1671a90d05
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-03-22 04:18:07 +0100

    disabled firefox extension testing because they have to be signed

--- test/UI/drivers/firefox-extension.ts
+++ test/UI/drivers/firefox-extension.ts
@@ -1,30 +1,44 @@
-import * as firefoxDriver from 'selenium-webdriver/firefox';
-import * as webdriver from 'selenium-webdriver';
+// import * as firefoxDriver from 'selenium-webdriver/firefox';
+// import * as webdriver from 'selenium-webdriver';
 import { TypedWebdriver } from '../../UITest';
 
-export function getCapabilities() {
-	const profile = new firefoxDriver.Profile();
-	profile.addExtension('dist/packed/Custom Right-Click Menu.xpi');
-	return new firefoxDriver.Options()
-		.setProfile(profile)
-		.toCapabilities();
+function throwErr(): never {
+	console.error('Firefox extension testing is not supported (yet), please try' +
+		' another browser or remove the --extensions flag');
+	process.exit(1);
+	throw new Error('Firefox extension testing is not supported (yet), please try' +
+		' another browser or remove the --extensions flag');
 }
 
-export async function openOptionsPage(driver: TypedWebdriver) {
-	await driver.get('about:debugging');
-	const addons = await driver.findElements(
-			webdriver.By.className('addon-target-container'));
-	for (const addon of addons) {
-		const title = await addon.findElement(
-			webdriver.By.className('target-name'));
-		if ((await title.getText()).indexOf('Custom Right-Click Menu') > -1) {
-			const uuid = await addon.findElement(
-				webdriver.By.css('.internal-uuid > *')).getText();
-			const optionsURL = `moz-extension://${uuid}/html/options.html`;
-			await driver.get(optionsURL);
-			return;
-		}
-	}
-	console.error('Failed to find extension options page');
-	process.exit(1);
+export function getCapabilities() {
+	// const profile = new firefoxDriver.Profile();
+	// profile.addExtension('dist/packed/Custom Right-Click Menu.xpi');
+	
+	// //Will only work when signed
+	// return new firefoxDriver.Options()
+	// 	.setProfile(profile)
+	// 	.toCapabilities();
+
+	return throwErr();
+}
+
+export async function openOptionsPage(_driver: TypedWebdriver) {
+	// await driver.get('about:debugging');
+	// const addons = await driver.findElements(
+	// 		webdriver.By.className('addon-target-container'));
+	// for (const addon of addons) {
+	// 	const title = await addon.findElement(
+	// 		webdriver.By.className('target-name'));
+	// 	if ((await title.getText()).indexOf('Custom Right-Click Menu') > -1) {
+	// 		const uuid = await addon.findElement(
+	// 			webdriver.By.css('.internal-uuid > *')).getText();
+	// 		const optionsURL = `moz-extension://${uuid}/html/options.html`;
+	// 		await driver.get(optionsURL);
+	// 		return;
+	// 	}
+	// }
+	// console.error('Failed to find extension options page');
+	// process.exit(1);
+
+	return throwErr();
 }
