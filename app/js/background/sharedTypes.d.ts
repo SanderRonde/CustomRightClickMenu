050dd9b457b53fc158c8d27e386922c1741e8f97
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-04-05 23:49:56 +0200

    add some documentation to the global object

--- app/js/background/sharedTypes.d.ts
+++ app/js/background/sharedTypes.d.ts
@@ -44,29 +44,86 @@ interface LogLineData {
 	hasResult?: boolean;
 }
 
+/**
+ * A log line listener
+ */
 type LogListener = (newLine: LogListenerLine) => void;
 
+/**
+ * Data about an active listener
+ */
 interface LogListenerObject {
+	/**
+	 * The listener itself
+	 */
 	listener: LogListener;
+	/**
+	 * The node ID for which to show messages
+	 */
 	id: number | string;
+	/**
+	 * The tab ID for which to show messages
+	 */
 	tab: number | string;
+	/**
+	 * A function that can be used to update the filters etc
+	 */
 	update: (id: string | number, tab: string | number, tabIndex: number, textFilter: string) => LogListenerLine[];
+	/**
+	 * The text to filter for
+	 */
 	text: string;
+	/**
+	 * The tab index to filter for
+	 */
 	index: number;
 }
 
+/**
+ * An active tab
+ */
 interface TabData {
+	/**
+	 * The ID of the tab
+	 */
 	id: number | 'background';
+	/**
+	 * The title of the tab
+	 */
 	title: string;
 }
 
+/**
+ * The contextmenu items in tree form
+ */
 interface ContextMenuItemTreeItem {
+	/**
+	 * The index of thisitem in its parent's children
+	 */
 	index: number;
+	/**
+	 * The ID of this item
+	 */
 	id: string|number;
+	/**
+	 * Whether this item is enabled
+	 */
 	enabled: boolean;
+	/**
+	 * The associated CRM node
+	 */
 	node: CRM.DividerNode | CRM.MenuNode | CRM.LinkNode | CRM.StylesheetNode | CRM.ScriptNode;
+	/**
+	 * The ID of the parent contextmenu item
+	 */
 	parentId: string|number;
+	/**
+	 * The children of this leaf
+	 */
 	children: ContextMenuItemTreeItem[];
+	/**
+	 * The children of this node's parent
+	 */
 	parentTree: ContextMenuItemTreeItem[];
 }
 
@@ -175,14 +232,38 @@ interface GreaseMonkeyData {
 	resources: CRMResources;
 }
 
+/**
+ * Logging data for the loggingpage
+ */
 interface MessageLogging {
+	/**
+	 * Logging by the node responsible for it
+	 */
 	[nodeId: number]: {
+		/**
+		 * Any log messages
+		 */
 		logMessages: LogListenerLine[];
+		/**
+		 * Stored values
+		 */
 		values: any[];
+		/**
+		 * Data by tab ID
+		 */
 		[tabId: number]: any;
 	};
+	/**
+	 * The active filter
+	 */
 	filter: {
+		/**
+		 * The node ID for which to show logs
+		 */
 		id: number;
+		/**
+		 * The tab ID for which to show logs
+		 */
 		tabId: number;
 	};
 }
@@ -242,46 +323,144 @@ interface MatchPattern {
 	invalid?: boolean;
 }
 
+/**
+ * Storage data
+ */
 interface BGStorages {
+	/**
+	 * All synced settings storage
+	 */
 	settingsStorage: CRM.SettingsStorage;
+	/**
+	 * URLs that should be globally excluded (nothing runs on them)
+	 */
 	globalExcludes: (MatchPattern|'<all_urls>')[];
+	/**
+	 * Registered resources
+	 */
 	resourceKeys: {
+		/**
+		 * The name of the resource
+		 */
 		name: string;
+		/**
+		 * The URL the resource points to
+		 */
 		sourceUrl: string;
+		/**
+		 * Hashes calculated over the content
+		 */
 		hashes: {
+			/**
+			 * The algorithm used to calculate the hash
+			 */
 			algorithm: string;
+			/**
+			 * The calculated hash
+			 */
 			hash: string;
 		}[];
+		/**
+		 * The script that uses it
+		 */
 		scriptId: number;
 	}[];
+	/**
+	 * An object with URLs as keys and resources as values
+	 */
 	urlDataPairs: {
+		/**
+		 * A resource data object
+		 */
 		[url: string]: {
+			/**
+			 * The data in string form
+			 */
 			dataString: string;
+			/**
+			 * All nodes that use this resource
+			 */
 			refs: number[];
+			/**
+			 * The original data URI
+			 */
 			dataURI: string;
 		};
 	};
+	/**
+	 * The local storage data (not synced)
+	 */
 	storageLocal: CRM.StorageLocal;
+	/**
+	 * Nodes' storage. Indexed by ID
+	 */
 	nodeStorage: {
+		/**
+		 * The storage for given node Id
+		 */
 		[nodeId: number]: any;
 	};
+	/**
+	 * Registered resources by the script ID
+	 */
 	resources: {
+		/**
+		 * A script's resources by name
+		 */
 		[scriptId: number]: {
+			/**
+			 * A resource object
+			 */
 			[name: string]: {
+				/**
+				 * The name of the resource
+				 */
 				name: string;
+				/**
+				 * The URL the resource points to
+				 */
 				sourceUrl: string;
+				/**
+				 * Whether the hashes match the content
+				 */
 				matchesHashes: boolean;
+				/**
+				 * The original data URI
+				 */
 				dataURI: string;
+				/**
+				 * The data in string form
+				 */
 				dataString: string;
+				/**
+				 * A url that points to this resource through a local non-existent.
+				 * For example: https://www.localhost.io/resource/${scriptId}/${name}
+				 */
 				crmUrl: string;
+				/**
+				 * Hashes calculated over the content
+				 */
 				hashes: {
+					/**
+					 * The algorithm used to calculate the hash
+					 */
 					algorithm: string;
+					/**
+					 * The calculated hash
+					 */
 					hash: string;
-				}[]
+				}[];
 			};
 		};
 	};
+	/**
+	 * Any messages about scripts trying to access permissions they don't have
+	 */
 	insufficientPermissions: string[];
+	/**
+	 * Tab lookups that failed as a result of inaccessible pages such as
+	 * chrome extension pages, file:// pages etc
+	 */
 	failedLookups: number[];
 }
 
@@ -293,19 +472,49 @@ interface SandboxWorkerInterface {
 	terminate(): void;
 }
 
+/**
+ * Data related to background pages
+ */
 interface BGBackground {
+	/**
+	 * The background pages ordered by node ID
+	 */
 	byId: {
+		/**
+		 * The sandbox worker for this node ID
+		 */
 		[nodeId: number]: SandboxWorkerInterface;
 	};
 }
 
+/**
+ * Data related to the CRM tree
+ */
 interface BGCRM {
+	/**
+	 * The CRM tree
+	 */
 	crmTree: (CRM.DividerNode | CRM.MenuNode | CRM.LinkNode | CRM.StylesheetNode | CRM.ScriptNode)[]
+	/**
+	 * CRM nodes ordered by ID
+	 */
 	crmById: {
+		/**
+		 * The CRM node for this ID
+		 */
 		[id: number]: CRM.DividerNode | CRM.MenuNode | CRM.LinkNode | CRM.StylesheetNode | CRM.ScriptNode;
 	};
+	/**
+	 * The CRM tree made safe (stripped of all sensitive data)
+	 */
 	safeTree: CRM.SafeNode[];
+	/**
+	 * Safe CRM nodes ordered by ID
+	 */
 	crmByIdSafe: {
+		/**
+		 * The safe CRM node for this ID
+		 */
 		[id: number]: CRM.SafeNode;
 	};
 }
@@ -340,98 +549,288 @@ interface ContextMenuSettings extends ContextMenuCreateProperties {
 	generatedId?: number;
 }
 
+/**
+ * Contextmenu nodes added by the user
+ */
 interface UserAddedContextMenu {
+	/**
+	 * The node that created this contextmenu item
+	 */
 	sourceNodeId: number;
+	/**
+	 * The properties that were used to create this contextmenu item
+	 */
 	createProperties: ContextMenuSettings;
+	/**
+	 * The mapped id used to identify this item. Not the actual
+	 * id as that changes with every re-creation
+	 */
 	generatedId: string|number;
+	/**
+	 * The actual contextmenu ID of this node
+	 */
 	actualId: string|number;
+	/**
+	 * The parent of this node
+	 */
 	parent: UserAddedContextMenu;
+	/**
+	 * The children of this node
+	 */
 	children: UserAddedContextMenu[];
 }
 
+/**
+ * Override settings for a contextmenu item
+ */
 interface ContextMenuOverrides {
+	/**
+	 * The new type of the contextmenu item
+	 */
 	type?: CRM.ContextMenuItemType;
+	/**
+	 * Whether this item should be checked
+	 */
 	checked?: boolean;
+	/**
+	 * The content types on which this item should appear
+	 */
 	contentTypes?: CRM.ContentTypeString[];
+	/**
+	 * Whether this item is visible
+	 */
 	isVisible?: boolean;
+	/**
+	 * Whether this item is disabled (greyed out)
+	 */
 	isDisabled?: boolean;
+	/**
+	 * The overridden name of this item
+	 */
 	name?: string;
 }
 
+/**
+ * Data related to the contextmenu and other on-page data
+ */
 interface BGCRMValues {
+	/**
+	 * Tab-specific data
+	 */
 	tabData: {
+		/**
+		 * The data for given tab
+		 */
 		[tabId: number]: {
+			/**
+			 * The nodes that are currently running on this tab by ID
+			 */
 			nodes: {
+				/**
+				 * An instance of a CRM script
+				 */
 				[nodeId: number]: {
+					/**
+					 * The secret key for this CRM API instance
+					 */
 					secretKey: number[];
+					/**
+					 * The runtime.port object associated with it
+					 */
 					port?: _browser.runtime.Port | {
 						postMessage(message: Object): void;
 					};
+					/**
+					 * Whether this instance uses local storage
+					 */
 					usesLocalStorage: boolean;
 				}[];
 			};
+			/**
+			 * Any libraries that are active on the tab by name
+			 */
 			libraries: {
+				/**
+				 * Whether given library is active on this page (true if it is)
+				 */
 				[library: string]: boolean;
 			};
 		};
 	};
+	/**
+	 * The ID of the root contextmenu item
+	 */
 	rootId: number|string;
+	/**
+	 * Contextmenu IDs by the node ID that created them
+	 */
 	contextMenuIds: {
+		/**
+		 * The ID of the contextmenu item
+		 */
 		[nodeId: number]: string|number;
 	};
+	/**
+	 * All active instances of nodes by node ID
+	 */
 	nodeInstances: {
+		/**
+		 * All instances of this node by their instance ID
+		 */
 		[nodeId: number]: {
+			/**
+			 * Instances by their instance ID
+			 */
 			[instanceId: number]: {
+				/**
+				 * Whether this instance has a handler associated with it yet
+				 */
 				hasHandler: boolean;
 			}[];
 		};
 	};
+	/**
+	 * Info about a contextmenu item by contextmenu item ID
+	 */
 	contextMenuInfoById: {
+		/**
+		 * Data about this contextmenu item
+		 */
 		[contextMenuId: number]: {
+			/**
+			 * The path to this item as an array of numbers
+			 */
 			path: number[];
+			/**
+			 * The settings used to create this contextmenu item
+			 */
 			settings: ContextMenuSettings;
+			/**
+			 * Whether this contextmenu item is enabled
+			 */
 			enabled: boolean;
 		};
+		/**
+		 * Data about this contextmenu item
+		 */
 		[contextMenuId: string]: {
+			/**
+			 * The path to this item as an array of numbers
+			 */
 			path: number[];
+			/**
+			 * The settings used to create this contextmenu item
+			 */
 			settings: ContextMenuSettings;
+			/**
+			 * Whether this contextmenu item is enabled
+			 */
 			enabled: boolean;
 		};
 	};
+	/**
+	 * The contextmenu items in tree form
+	 */
 	contextMenuItemTree: ContextMenuItemTreeItem[];
+	/**
+	 * Contextmenu nodes added by the user
+	 */
 	userAddedContextMenus: UserAddedContextMenu[];
+	/**
+	 * Contextmenus added by the user sorted by their mapped ID
+	 */
 	userAddedContextMenusById: {
+		/**
+		 * The contextmenu item that was added by the user
+		 */
 		[mappedId: string]: UserAddedContextMenu;
+		/**
+		 * The contextmenu item that was added by the user
+		 */
 		[mappedId: number]: UserAddedContextMenu;
 	},
+	/**
+	 * Contextmenu settings overrides for items on all tabs (global) by the node ID
+	 */
 	contextMenuGlobalOverrides: {
+		/**
+		 * The override settings
+		 */
 		[nodeId: number]: ContextMenuOverrides;
 	},
+	/**
+	 * Data about on which pages to show or hide contextmenu items by ID
+	 */
 	hideNodesOnPagesData: {
+		/**
+		 * A configuration about the URL and whether to hide the node on it
+		 */
 		[nodeId: number]: {
+			/**
+			 * Whether to not show the node on given URL
+			 */
 			not: boolean;
+			/**
+			 * The URL on which to show or not show the node
+			 */
 			url: string;
 		}[];
 	};
+	/**
+	 * Info about the status of a node on given tab by ID
+	 */
 	nodeTabStatuses: {
+		/**
+		 * The data about given node by tab ID
+		 */
 		[nodeId: number]: {
+			/**
+			 * Data by each tab the node is running in
+			 */
 			[tabId: number]: {
+				/**
+				 * Whether the checkbox is checked
+				 */
 				checked?: boolean;
+				/**
+				 * Tab-specific contextmenu overrides
+				 */
 				overrides?: ContextMenuOverrides;
 			};
+			/**
+			 * The default checked status of the node
+			 */
 			defaultCheckedValue?: boolean;
 		};
 	};
 }
 
+/**
+ * On what pages and when which nodes need to be executed
+ */
 interface BGToExecute {
+	/**
+	 * Data about whether to show or to not show nodes on given URLs by node ID
+	 */
 	onUrl: {
+		/**
+		 * The URLs on which to show or not to show this node
+		 */
 		[nodeId: number]: CRM.Trigger[];
 	};
+	/**
+	 * The script nodes that should be executed on document start (early)
+	 */
 	documentStart: CRM.ScriptNode[];
+	/**
+	 * Nodes that should always be executed (not early)
+	 */
 	always: (CRM.DividerNode | CRM.MenuNode | CRM.LinkNode | CRM.StylesheetNode | CRM.ScriptNode)[];
 }
 
+/**
+ * A function used to send a response as a callback to a page
+ */
 type SendCallbackMessage = (tabId: number, tabIndex: number, id: number, data: {
 	err: boolean,
 	errorMessage?: string;
@@ -440,34 +839,107 @@ type SendCallbackMessage = (tabId: number, tabIndex: number, id: number, data: {
 }) => void;
 
 interface Globals {
+	/**
+	 * The last ID to be generated for a node
+	 */
 	latestId: number;
+	/**
+	 * Storage data
+	 */
 	storages: BGStorages;
+	/**
+	 * Data related to background pages
+	 */
 	background: BGBackground;
+	/**
+	 * Data related to the CRM tree
+	 */
 	crm: BGCRM;
+	/**
+	 * Permissions available to this extension
+	 */
 	availablePermissions: string[];
+	/**
+	 * Data related to the contextmenu and other on-page data
+	 */
 	crmValues: BGCRMValues;
+	/**
+	 * On what pages and when which nodes need to be executed
+	 */
 	toExecuteNodes: BGToExecute;
+	/**
+	 * A function used to send a response as a callback to a page
+	 */
 	sendCallbackMessage: SendCallbackMessage;
+	/**
+	 * Global event listeners
+	 */
 	eventListeners: {
+		/**
+		 * Event listeners for the browser.notifications API
+		 */
 		notificationListeners: {
+			/**
+			 * The notification's data
+			 */
 			[notificationId: string]: {
+				/**
+				 * The ID of the node that created the notification
+				 */
 				id: number;
+				/**
+				 * The ID of the tab from which a node created the notification
+				 */
 				tabId: number;
+				/**
+				 * The index of the script on the tab that created it (if 3 instances
+				 * of the script are running, their tabIndexes are 0, 1 and 2 respectively)
+				 */
 				tabIndex: number;
+				/**
+				 * The ID of the notification
+				 */
 				notificationId: number;
+				/**
+				 * A callback ID for an onDone listener
+				 */
 				onDone: number;
+				/**
+				 * A callback ID for an onClick listener
+				 */
 				onClick: number;
 			};
 		};
+		/**
+		 * Event listeners for the browser's keyboard shortcut API
+		 */
 		shortcutListeners: {
+			/**
+			 * The keyboard shortcut's description
+			 */
 			[shortcut: string]: {
+				/**
+				 * The keyboard shortcut (as keys)
+				 */
 				shortcut: string;
+				/**
+				 * The function to run when it's pressed
+				 */
 				callback(): void;
 			}[];
 		};
 	};
+	/**
+	 * Data about script logging for the logging page
+	 */
 	logging: MessageLogging;
+	/**
+	 * Any constant values
+	 */
 	constants: BGConstants;
+	/**
+	 * Listeners for the logging page
+	 */
 	listeners: BGListeners;
 }
 
@@ -490,26 +962,77 @@ interface CRMTemplates {
 }
 
 interface BGConstants {
+	/**
+	 * The supported hashing functions
+	 */
 	supportedHashes: string[];
+	/**
+	 * Valid protocols
+	 */
 	validSchemes: string[]; 
+	/**
+	 * Templates used to generate new nodes
+	 */
 	templates: CRMTemplates;
+	/**
+	 * A JSON function used to encode and decode functions as well
+	 */
 	specialJSON: SpecialJSON;
+	/**
+	 * Valid permissions for this extension
+	 */
 	permissions: CRM.Permission[];
+	/**
+	 * Valid page contexts
+	 */
 	contexts: _browser.contextMenus.ContextType[];
+	/**
+	 * The chrome extension IDs for the tampermonkey extensions (beta and normal)
+	 */
 	tamperMonkeyExtensions: string[];
 }
 
+/**
+ * Listeners for the logging page
+ */
 interface BGListeners {
+	/**
+	 * The active node IDs and their titles
+	 */
 	idVals: {
+		/**
+		 * The ID of this node
+		 */
 		id: number;
+		/**
+		 * The name of this node
+		 */
 		title: string;
 	}[];
+	/**
+	 * The active tabs
+	 */
 	tabVals: TabData[];
+	/**
+	 * A function that can be used to register an on ID update handler
+	 */
 	ids: ((updatedIds: {
+		/**
+		 * The node ID
+		 */
 		id: number;
+		/**
+		 * The name of the node
+		 */
 		title: string;
 	}[]) => void)[];
-	tabs: ((updatedTabs: TabData[]) => void)[]
+	/**
+	 * A function that can be used to register an on tab ID update handler
+	 */
+	tabs: ((updatedTabs: TabData[]) => void)[];
+	/**
+	 * Any log listeners and their filters
+	 */
 	log: LogListenerObject[];
 }
 
