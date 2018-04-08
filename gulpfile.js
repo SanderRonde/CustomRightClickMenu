ae8324588fd11daf1aa6415519d53dec24800eb7
SanderRonde <awsdfgvhbjn@gmail.com>
2018-04-08 02:34:55 +0200

    split ts files

--- gulpfile.js
+++ gulpfile.js
@@ -230,7 +230,7 @@ function readFile(filePath, options) {
 				const project = ts.createProject('tsconfig.json');
 				return project.src()
 					.pipe(project())
-					.js.pipe(gulp.dest('./'));
+					.js.pipe(gulp.dest('./app'));
 			},
 			function compileTest() {
 				const project = ts.createProject('test/tsconfig.json');
