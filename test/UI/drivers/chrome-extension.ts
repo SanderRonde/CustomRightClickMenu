746661bfc090b9a3f8b3fc9f9f91aa4dafe7d781
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-05-01 22:40:05 +0200

    fix extension page for chrome > 66

--- test/UI/drivers/chrome-extension.ts
+++ test/UI/drivers/chrome-extension.ts
@@ -1,4 +1,4 @@
-import { TypedWebdriver, BrowserstackCapabilities } from '../../UITest';
+import { TypedWebdriver, BrowserstackCapabilities, wait, inlineFn, findElement } from '../../UITest';
 import * as chromeDriver from 'selenium-webdriver/chrome';
 import * as webdriver from 'selenium-webdriver';
 
@@ -30,17 +30,33 @@ export async function getExtensionURLPrefix(driver: TypedWebdriver, capabilities
 	} else {
 		await driver.get('chrome://extensions');
 	}
-	const extensions = await driver.findElements(
-		webdriver.By.className('extension-list-item-wrapper'));
-	for (const extension of extensions) {
-		const title = await extension.findElement(
-			webdriver.By.className('extension-title'));
-		if ((await title.getText()).indexOf('Custom Right-Click Menu') > -1) {
-			const href = await extension
-				.findElement(webdriver.By.className('options-link'))
-				.getAttribute('href');
-			
-			return href.split('/options.html')[0];
+	if (version < 66) {
+		const extensions = await driver.findElements(
+			webdriver.By.className('extension-list-item-wrapper'));
+		for (const extension of extensions) {
+			const title = await extension.findElement(
+				webdriver.By.className('extension-title'));
+			if ((await title.getText()).indexOf('Custom Right-Click Menu') > -1) {
+				const href = await extension
+					.findElement(webdriver.By.className('options-link'))
+					.getAttribute('href');
+				
+				return href.split('/options.html')[0];
+			}
+		}
+	} else {
+		const extensions = await findElement(webdriver.By.tagName('extensions-manager'))
+			.findElement(webdriver.By.tagName('extensions-item-list'))
+			.findElements(webdriver.By.tagName('extensions-item'));
+		for (const extension of extensions) {
+			const title = await extension.findElement(webdriver.By.id('name'));
+			if ((await title.getText()).indexOf('Custom Right-Click Menu') > -1) {
+				await extension.findElement(webdriver.By.id('details-button')).click();
+				await wait(1000);
+				return `chrome-extension://${await driver.executeScript(inlineFn(() => {
+					return location.href.split('?id=')[1];
+				}))}`;
+			}
 		}
 	}
 	console.error('Failed to find extension options page');
@@ -62,27 +78,58 @@ export async function openOptionsPage(driver: TypedWebdriver, capabilities: Brow
 	} else {
 		await driver.get('chrome://extensions');
 	}
-	const extensions = await driver.findElements(
-		webdriver.By.className('extension-list-item-wrapper'));
-	for (const extension of extensions) {
-		const title = await extension.findElement(
-			webdriver.By.className('extension-title'));
-		if ((await title.getText()).indexOf('Custom Right-Click Menu') > -1) {
-			await extension
-				.findElement(webdriver.By.className('options-link'))
-				.click();
-			const currentTab = await driver.getWindowHandle();
-			const tabs = await driver.getAllWindowHandles();
-			const nonCurrentTabs = tabs.filter((tab) => {
-				return tab !== currentTab;
-			});
-			
-			//Close the curent tab
-			await driver.close();
+	await wait(500);
+	if (version < 66) {
+		const extensions = await driver.findElements(
+			webdriver.By.className('extension-list-item-wrapper'));
+		for (const extension of extensions) {
+			const title = await extension.findElement(
+				webdriver.By.className('extension-title'));
+			if ((await title.getText()).indexOf('Custom Right-Click Menu') > -1) {
+				await extension
+					.findElement(webdriver.By.className('options-link'))
+					.click();
+				const currentTab = await driver.getWindowHandle();
+				const tabs = await driver.getAllWindowHandles();
+				const nonCurrentTabs = tabs.filter((tab) => {
+					return tab !== currentTab;
+				});
+				
+				//Close the curent tab
+				await driver.close();
 
-			//Switch to next tab
-			await driver.switchTo().window(nonCurrentTabs[0]);
-			return;
+				//Switch to next tab
+				await driver.switchTo().window(nonCurrentTabs[0]);
+				return;
+			}
+		}
+	} else {
+		const extensions = await findElement(webdriver.By.tagName('extensions-manager'))
+			.findElement(webdriver.By.tagName('extensions-item-list'))
+			.findElements(webdriver.By.tagName('extensions-item'));
+		for (const extension of extensions) {
+			const title = await extension.findElement(webdriver.By.id('name'));
+			if ((await title.getText()).indexOf('Custom Right-Click Menu') > -1) {
+				await extension.findElement(webdriver.By.id('details-button')).click();
+				await wait(2000);
+				await findElement(webdriver.By.tagName('extensions-manager'))
+					.findElement(webdriver.By.tagName('extensions-detail-view'))
+					.findElement(webdriver.By.id('extensions-options'))
+					.click();
+
+				const currentTab = await driver.getWindowHandle();
+				const tabs = await driver.getAllWindowHandles();
+				const nonCurrentTabs = tabs.filter((tab) => {
+					return tab !== currentTab;
+				});
+				
+				//Close the curent tab
+				await driver.close();
+
+				//Switch to next tab
+				await driver.switchTo().window(nonCurrentTabs[0]);
+				return;
+			}
 		}
 	}
 	console.error('Failed to find extension options page');
