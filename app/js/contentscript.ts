e6a76f1ce7ce77c94acd095d645a62ebcfd245a4
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-05-02 23:36:44 +0200

    change contentscript to .ts file

--- app/js/contentscript.ts
+++ app/js/contentscript.ts
@@ -1,12 +1,12 @@
-﻿(function() {
-	function hacksecuteScript(script) {
+﻿(() => {
+	function hacksecuteScript(script: string) {
 		var tag = document.createElement('script');
 		tag.innerHTML = script;
 		document.documentElement.appendChild(tag);
 		document.documentElement.removeChild(tag);
 	}
 
-	function fetchFile(file, callback) {
+	function fetchFile(file: string, callback: (response: string) => void) {
 		//Get code
 		var xhr = new XMLHttpRequest();
 		xhr.open('GET', file);
@@ -22,7 +22,7 @@
 		xhr.send();
 	}
 
-	function runCRMAPI(callback) {
+	function runCRMAPI(callback: () => void) {
 		//Get CRM API URL
 		var url = browserAPI.runtime.getURL('/js/crmapi.js');
 		fetchFile(url, function(code) {
@@ -31,7 +31,10 @@
 		});
 	}
 
-	function executeScript(scripts, index) {
+	function executeScript(scripts: {
+		code?: string;
+		file?: string;
+	}[], index: number = 0) {
 		if (scripts.length > index) {
 			if (scripts[index].code) {
 				hacksecuteScript(scripts[index].code);
@@ -49,18 +52,33 @@
 		}
 	}
 
-	function executeScripts(scripts) {
-		executeScript(scripts, 0);
+	type Writable<T> = {
+		-readonly [P in keyof T]: T[P];
 	}
 
 	var matched = false;
 	var contextMenuEventKeys = ['clientX', 'clientY', 'offsetX',' offsetY', 'pageX', 'pageY', 'screenX',
 		'screenY', 'which', 'x', 'y']
-	var lastContextmenuCall = null;
+	var lastContextmenuCall: PointerEvent = null;
 	var contextElementId = 1;
 
 	var crmAPIExecuted = false;
-	browserAPI.runtime.onMessage.addListener(function (message, sender, respond) {
+	browserAPI.runtime.onMessage.addListener(function (message: {
+		type: 'checkTabStatus';
+		data: {
+			willBeMatched: boolean;
+		}
+	} & {
+		type: 'getLastClickInfo';	
+	} & {
+		type: 'runScript';
+		data: {
+			scripts: {
+				code?: string;
+				file?: string;
+			}[];
+		}	
+	}, sender: _browser.runtime.MessageSender, respond: (response: any) => Promise<void>) {
 		switch (message.type) {
 			case 'checkTabStatus':
 				//Code was already executed here, check if it has been matched before
@@ -72,10 +90,17 @@
 				}
 				break;
 			case 'getLastClickInfo':
-				var responseObj = {};
+				var responseObj = ({} as Partial<PointerEvent>) as Writable<PointerEvent> & {
+					srcElement: number;
+					target: number;
+					toElement: number;
+				}
 				for (var key in lastContextmenuCall) {
 					if (contextMenuEventKeys.indexOf(key) !== -1) {
-						responseObj[key] = lastContextmenuCall[key];
+						const pointerKey = key as keyof PointerEvent;
+						if (pointerKey !== 'button') {
+							responseObj[pointerKey] = lastContextmenuCall[pointerKey];
+						}
 					}
 				}
 
@@ -83,22 +108,22 @@
 					respond(null);
 				} else {
 					lastContextmenuCall.srcElement.classList.add('crm_element_identifier_' + ++contextElementId);
-					responseObj.srcElement = contextElementId;
-					lastContextmenuCall.target.classList.add('crm_element_identifier_' + ++contextElementId);
-					responseObj.target = contextElementId;
+					responseObj.srcElement = contextElementId as Element & number;
+					(lastContextmenuCall.target as HTMLElement).classList.add('crm_element_identifier_' + ++contextElementId);
+					responseObj.target = contextElementId as Element & number;
 					lastContextmenuCall.toElement.classList.add('crm_element_identifier_' + ++contextElementId);
-					responseObj.toElement = contextElementId;
+					responseObj.toElement = contextElementId as Element & number;
 					respond(responseObj);
 				}
 				break;
 			case 'runScript':
 				if (!crmAPIExecuted) {
 					runCRMAPI(function() {
-						executeScripts(message.data.scripts);
+						executeScript(message.data.scripts);
 					});
 					crmAPIExecuted = true;
 				} else {
-					executeScripts(message.data.scripts);
+					executeScript(message.data.scripts);
 				}
 				break;
 		}
@@ -106,7 +131,9 @@
 
 	browserAPI.runtime.sendMessage({
 		type: 'newTabCreated'
-	}).then(function(response) {
+	}).then(function(response: {
+		matched?: boolean;
+	}) {
 		if (response && response.matched) {
 			matched = true;
 		}
@@ -116,7 +143,7 @@
 		if (result.useAsUserscriptInstaller) {
 			var installURL = browserAPI.runtime.getURL('html/install.html');
 			document.body.addEventListener('mousedown', function(e) {
-				var target = e.target;
+				var target = e.target as HTMLAnchorElement;
 				if (target && target.href && target.href.indexOf(installURL) === -1 && target.href.match(/.+user\.js$/)) {
 					var installPageURL = installURL + '?i=' + encodeURIComponent(target.href) + '&s=' +
 						encodeURIComponent(location.href);
