d10a651c9619d38b0cbd4addebd7b3605abb7d4c
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-05-25 21:14:40 +0200

    attempt selected tests multiple times

---
+++ scripts/do_rerun.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [ "$1" = "test:chrome:old:install:greasyfork" ] || [ "$1" = "test:chrome:old:install:openuserjs" ] || [ "$1" = "test:chrome:old:install:userscriptsorg" ] || [ "$1" = "test:chrome:old:install:userstylesorg" ] ; then
+	yarn run $TEST || yarn run $TEST
+fi
