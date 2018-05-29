70e68cbc5437bfee9f6cff8db61f0283b67d761b
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-05-29 18:51:31 +0200

    don't include all webcomponents libs

--- gulpfile.js
+++ gulpfile.js
@@ -462,7 +462,7 @@ function readFile(filePath, options) {
 			function copyWebcomponentsLibs() {
 				return gulp
 					.src([
-						'webcomponentsjs/**/*.js',
+						'webcomponentsjs/*.js',
 						'!webcomponentsjs/webcomponents-lite.js'
 					], { 
 						cwd: './temp/bower_components', 
