6abda4deb12aea8dbdced42cdc262aa23256e37f
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-06-11 22:31:32 +0200

    retry select tests only once

--- scripts/do_test.sh
+++ scripts/do_test.sh
@@ -10,24 +10,36 @@ $script_path/is_eq.sh "$TRAVIS_PULL_REQUEST" "false" && run_test=1
 $script_path/is_eq.sh "$1" "test-build" && run_test=1
 $script_path/is_eq.sh "$1" "test-local" && run_test=1
 
-# Retry the install tests
-do_retry=0
-$script_path/is_eq.sh "$1" "test:chrome:old:install:greasyfork" && do_retry=1
-$script_path/is_eq.sh "$1" "test:chrome:old:install:openuserjs" && do_retry=1
-$script_path/is_eq.sh "$1" "test:chrome:old:install:userscriptsorg" && do_retry=1
-$script_path/is_eq.sh "$1" "test:chrome:old:install:userstylesorg" && do_retry=1
-$script_path/is_eq.sh "$1" "test-migration:2.0.12" && do_retry=1
-$script_path/is_eq.sh "$1" "test-migration:2.0.13" && do_retry=1
-$script_path/is_eq.sh "$1" "test-migration:2.0.14" && do_retry=1
-$script_path/is_eq.sh "$1" "test-migration:2.0.15" && do_retry=1
-$script_path/is_eq.sh "$1" "test-migration:2.0.16" && do_retry=1
-$script_path/is_eq.sh "$1" "test-migration:2.0.17" && do_retry=1
-$script_path/is_eq.sh "$1" "test-migration:2.0.18" && do_retry=1
-$script_path/is_eq.sh "$1" "test-migration:2.0.19" && do_retry=1
+do_retry_once=0
+$script_path/is_eq.sh "$1" "test:chrome:old:page" && do_retry_once=1
+$script_path/is_eq.sh "$1" "test:chrome:old:extension" && do_retry_once=1
+$script_path/is_eq.sh "$1" "test:chrome:latest:page" && do_retry_once=1
+$script_path/is_eq.sh "$1" "test:chrome:latest:extension" && do_retry_once=1
+$script_path/is_eq.sh "$1" "test:firefox:quantum" && do_retry_once=1
+$script_path/is_eq.sh "$1" "test:firefox:latest" && do_retry_once=1
+$script_path/is_eq.sh "$1" "test:edge:16" && do_retry_once=1
+$script_path/is_eq.sh "$1" "test:edge:latest" && do_retry_once=1
+
+# Retry some tests
+do_retry_twice=0
+$script_path/is_eq.sh "$1" "test:chrome:old:install:greasyfork" && do_retry_twice=1
+$script_path/is_eq.sh "$1" "test:chrome:old:install:openuserjs" && do_retry_twice=1
+$script_path/is_eq.sh "$1" "test:chrome:old:install:userscriptsorg" && do_retry_twice=1
+$script_path/is_eq.sh "$1" "test:chrome:old:install:userstylesorg" && do_retry_twice=1
+$script_path/is_eq.sh "$1" "test-migration:2.0.12" && do_retry_twice=1
+$script_path/is_eq.sh "$1" "test-migration:2.0.13" && do_retry_twice=1
+$script_path/is_eq.sh "$1" "test-migration:2.0.14" && do_retry_twice=1
+$script_path/is_eq.sh "$1" "test-migration:2.0.15" && do_retry_twice=1
+$script_path/is_eq.sh "$1" "test-migration:2.0.16" && do_retry_twice=1
+$script_path/is_eq.sh "$1" "test-migration:2.0.17" && do_retry_twice=1
+$script_path/is_eq.sh "$1" "test-migration:2.0.18" && do_retry_twice=1
+$script_path/is_eq.sh "$1" "test-migration:2.0.19" && do_retry_twice=1
 
 if ((run_test)); then
-	if ((do_retry)); then
+	if ((do_retry_twice)); then
 		yarn run $1 || yarn run $1 || yarn run $1
+	elif ((do_retry_once)); then
+		yarn run $1 || yarn run $1
 	else
 		yarn run $1
 	fi
