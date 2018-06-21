8859987cc26efd9f148bce49feefe34cbed9682d
Sander Ronde <awsdfgvhbjn@gmail.com>
2018-06-21 03:53:53 +0200

    fix exit code

--- scripts/is_latest_commit.sh
+++ scripts/is_latest_commit.sh
@@ -5,6 +5,5 @@ script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 latest_commit=`git ls-remote git://github.com/SanderRonde/CustomRightClickMenu.git | grep HEAD | grep -oP "^[A-Za-z0-9]*"`
 current_commit="$TRAVIS_COMMIT"
 
-is_same=`$script_path/is_eq.sh $latest_commit $current_commit`
-
-exit is_same
+$script_path/is_eq.sh $latest_commit $current_commit || exit 1
+exit 0
