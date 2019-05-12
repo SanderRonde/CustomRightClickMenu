24fe0e6818e8d25eb9f603ff476bd967d9e1a1c8
Sander Ronde <awsdfgvhbjn@gmail.com>
2019-05-12 17:45:08 +0200

    implement paper-toggle-option

--- app/wc-elements/options/inputs/paper-toggle-option/paper-toggle-option.css.ts
+++ app/wc-elements/options/inputs/paper-toggle-option/paper-toggle-option.css.ts
@@ -1,9 +1,10 @@
+import { PaperMaterialStyles } from '../../../mixins/styles/paper-material-styles.css.js';
 import { TemplateFn, CHANGE_TYPE } from '../../../../modules/wclib/build/es/wclib.js';
 import { render } from '../../../../modules/lit-html/lit-html.js';
 import { PaperToggleOption } from './paper-toggle-option.js';
 
-export const PaperToggleOptionCSS = new TemplateFn<PaperToggleOption>(function (html) {
+export const PaperToggleOptionCSS = new TemplateFn<PaperToggleOption>(function (html, _props, _theme, change) {
 	return html`<style>
-		
+		${PaperMaterialStyles.renderSame(change, this, html)}
 	</style>`
 }, CHANGE_TYPE.THEME, render);
--- app/wc-elements/options/inputs/paper-toggle-option/paper-toggle-option.ts
+++ app/wc-elements/options/inputs/paper-toggle-option/paper-toggle-option.ts
@@ -2,6 +2,9 @@ import { ConfigurableWebComponent, Props, PROP_TYPE, config } from '../../../../
 import { PaperToggleOptionIDMap, PaperToggleOptionClassMap } from './paper-toggle-option-querymap';
 import { PaperToggleOptionHTML } from './paper-toggle-option.html.js';
 import { PaperToggleOptionCSS } from './paper-toggle-option.css.js';
+import { CRMWindow } from '../../../defs/crm-window.js';
+
+declare const window: CRMWindow;
 
 @config({
 	is: 'paper-toggle-option',
@@ -13,14 +16,37 @@ export class PaperToggleOption extends ConfigurableWebComponent<{
 	CLASSES: PaperToggleOptionClassMap;
 }> {
 	props = Props.define(this, {
-		// ...
+		reflect: {
+			toggled: {
+				type: PROP_TYPE.BOOL,
+				value: false
+			},
+			disabled: {
+				type: PROP_TYPE.BOOL,
+				value: false
+			},
+			disabledreason: {
+				type: PROP_TYPE.STRING,
+				value: 'disabled'
+			},
+			showmessage: {
+				type: PROP_TYPE.BOOL,
+				value: false
+			}
+		}
 	});
 
-	mounted() {
-		// ...
-	}
+	setCheckboxDisabledValue(this: PaperToggleOption, value: boolean) {
+		this.$.checkbox.disabled = value;
+		this.disabled = value;
+	};
 
-	firstRender() {
-		// ...
+	init(this: PaperToggleOption, storage: CRM.StorageLocal) {
+		this.props.toggled = storage[this.getAttribute('id') as keyof CRM.StorageLocal] as boolean;
+	};
+
+	onClick(this: PaperToggleOption) {
+		const id = this.getAttribute('id') as keyof CRM.StorageLocal;
+		window.app.setLocal(id, !this.props.toggled);
 	}
 }
